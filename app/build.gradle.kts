98fc39b2bbaedfdde729363b4ff487eb83572866
Xsinh <funnybalmosh@gmail.com>
2025-01-08 11:59:28 +0300

    - update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
 
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
